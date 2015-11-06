8c0a6bac8bca9adfb51988edcee85d0275e3dc93
許菀庭 <cindyemail0720@yahoo.com.tw>
2015-11-06 20:01:37 +0800

    Cindy's first commit

---
+++ h/PositionImgSetting.h
@@ -0,0 +1,50 @@
+#ifndef _POSITIONSETTING_H_
+#define _POSITIONSETTING_H_
+#include "Image.h" 
+#include <string>
+using namespace std;
+
+
+class PositionImgSetting {
+public:
+	PositionImgSetting();
+	PositionImgSetting(string&, int);	//給room name 和 position編號
+
+	void set_room_name(int);
+	string& get_room_name();	
+	void set_number(int);
+	int get_number();	//查看位置編號	
+	Mat get_image(int);	//讀檔查看編號n的圖
+	
+	void StitchScreen(int, int, int);	//輸入數字n，拼接圖片編號(n,n+1,n+2,n+12,n+13,n+14,n+24,n+25,n+26)，並輸出存在image/position_number/stitch_n.jpg
+	void StitchScreenAll(int);	//把360張stitch9都拼接出來，並輸出存在image/position_number/stitch_1.jpg~stitch_36.jpg
+	
+	//stitch完可以做測試用
+	void draw_2img_matches(Mat, Mat, int);
+	//測試在PositionSetting做好，position為了不浪費記憶體一次只讀一張圖
+	Mat GetStitchScreen(int);	//讀檔image/position_number/n.jpg
+	//Mat get_screen(int, int);	//輸入水平及垂直角度，得到視窗長相
+	void show_screen();		//秀出完整視窗，包括拖曳功能
+
+private:
+	string _room_name;
+	int _number; // 位置編號
+
+	Mat stitch2(Mat, Mat, int, int, vector<int> &);
+	int getImgAmount(string);
+	void Initial3Img(int, int, int);	//stitch_part 代表上方 中間 或下方
+	void StitchPart(int, int, int, int, vector<int> &, Mat&);
+	
+	void draw_matches(Mat, Mat, vector< KeyPoint >, vector< KeyPoint >, vector< DMatch >, int);
+	void symmetryTest(const vector<DMatch>, const vector<DMatch>, vector<DMatch>&);
+	vector< DMatch > get_good_dist_matches( vector< DMatch >, int);
+	vector< DMatch > get_correct_range_matches(vector<DMatch>, Image, Image, int);
+	vector< DMatch > get_correct_range_matches_vertical(vector<DMatch>, Image, Image, int);
+	
+	void get_homography_matrix(Image, Image, vector< DMatch >, Mat&);
+	Mat get_blending_matrix(Mat, Mat, Mat);
+	Mat get_stitch_matrix(Mat, Mat, int, Mat, vector<int> &);
+	//Mat make_screen(int, int, int);	//輸入編號為多少的stitch9的圖，以及水平轉幾度，垂直轉幾度
+};
+
+#endif
---
+++ h/PyramidBlending.h
@@ -0,0 +1,123 @@
+#include<iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+#include <cv.h>
+#include <cxcore.h>
+#include <cvaux.h>
+
+
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+/************************************************************************/
+/* ?明：
+*金字塔?下到上依次? [0,1，...，level-1] ?
+*blendMask ??像的掩模
+*maskGaussianPyramid?金字塔每一?的掩模
+*resultLapPyr 存放每?金字塔中直接用左右??Laplacian??拼成的?像
+*/
+/************************************************************************/
+
+
+class LaplacianBlending {
+private:
+	Mat_<Vec3f> left;
+	Mat_<Vec3f> right;
+	Mat_<float> blendMask;
+
+	vector<Mat_<Vec3f> > leftLapPyr, rightLapPyr, resultLapPyr;//Laplacian Pyramids
+	Mat leftHighestLevel, rightHighestLevel, resultHighestLevel;
+	vector<Mat_<Vec3f> > maskGaussianPyramid; //masks are 3-channels for easier multiplication with RGB
+
+	int levels;
+
+	void buildPyramids() {
+		buildLaplacianPyramid(left, leftLapPyr, leftHighestLevel);
+		buildLaplacianPyramid(right, rightLapPyr, rightHighestLevel);
+		buildGaussianPyramid();
+	}
+
+	void buildGaussianPyramid() {//金字塔?容?每一?的掩模
+		assert(leftLapPyr.size()>0);
+
+		maskGaussianPyramid.clear();
+		Mat currentImg;
+		cvtColor(blendMask, currentImg, CV_GRAY2BGR);//store color img of blend mask into maskGaussianPyramid
+		maskGaussianPyramid.push_back(currentImg); //0-level
+
+		currentImg = blendMask;
+		for (int l = 1; l<levels + 1; l++) {
+			Mat _down;
+			if (leftLapPyr.size() > l)
+				pyrDown(currentImg, _down, leftLapPyr[l].size());
+			else
+				pyrDown(currentImg, _down, leftHighestLevel.size()); //lowest level
+
+			Mat down;
+			cvtColor(_down, down, CV_GRAY2BGR);
+			maskGaussianPyramid.push_back(down);//add color blend mask into mask Pyramid
+			currentImg = _down;
+		}
+	}
+
+	void buildLaplacianPyramid(const Mat& img, vector<Mat_<Vec3f> >& lapPyr, Mat& HighestLevel) {
+		lapPyr.clear();
+		Mat currentImg = img;
+		for (int l = 0; l<levels; l++) {
+			Mat down, up;
+			pyrDown(currentImg, down);
+			pyrUp(down, up, currentImg.size());
+			Mat lap = currentImg - up;
+			lapPyr.push_back(lap);
+			currentImg = down;
+		}
+		currentImg.copyTo(HighestLevel);
+	}
+
+	Mat_<Vec3f> reconstructImgFromLapPyramid() {
+		//?左右laplacian?像拼成的resultLapPyr金字塔中每一?
+		//?上到下插值放大并相加，即得blend?像?果
+		Mat currentImg = resultHighestLevel;
+		for (int l = levels - 1; l >= 0; l--) {
+			Mat up;
+
+			pyrUp(currentImg, up, resultLapPyr[l].size());
+			currentImg = up + resultLapPyr[l];
+		}
+		return currentImg;
+	}
+
+	void blendLapPyrs() {
+		//?得每?金字塔中直接用左右??Laplacian??拼成的?像resultLapPyr
+		resultHighestLevel = leftHighestLevel.mul(maskGaussianPyramid.back()) +
+			rightHighestLevel.mul(Scalar(1.0, 1.0, 1.0) - maskGaussianPyramid.back());
+		for (int l = 0; l<levels; l++) {
+			Mat A = leftLapPyr[l].mul(maskGaussianPyramid[l]);
+			Mat antiMask = Scalar(1.0, 1.0, 1.0) - maskGaussianPyramid[l];
+			Mat B = rightLapPyr[l].mul(antiMask);
+			Mat_<Vec3f> blendedLevel = A + B;
+
+			resultLapPyr.push_back(blendedLevel);
+		}
+	}
+
+public:
+	LaplacianBlending(const Mat_<Vec3f>& _left, const Mat_<Vec3f>& _right, const Mat_<float>& _blendMask, int _levels) ://construct function, used in LaplacianBlending lb(l,r,m,4);
+		left(_left), right(_right), blendMask(_blendMask), levels(_levels)
+	{
+		assert(_left.size() == _right.size());
+		assert(_left.size() == _blendMask.size());
+		buildPyramids();	//construct Laplacian Pyramid and Gaussian Pyramid
+		blendLapPyrs();	//blend left & right Pyramids into one Pyramid
+	};
+
+	Mat_<Vec3f> blend() {
+		return reconstructImgFromLapPyramid();//reconstruct Image from Laplacian Pyramid
+	}
+};
+
+Mat_<Vec3f> LaplacianBlend(const Mat_<Vec3f>& l, const Mat_<Vec3f>& r, const Mat_<float>& m) {
+	LaplacianBlending lb(l, r, m, 6);
+	return lb.blend();
+}
