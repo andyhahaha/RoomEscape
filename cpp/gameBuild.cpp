abbd54ec260cb47fe49b48280da0e37b9e6b4937
許菀庭 <cindyemail0720@yahoo.com.tw>
2015-11-20 01:24:39 +0800

    cindy update choose iimg

--- cpp/gameBuild.cpp
+++ cpp/gameBuild.cpp
@@ -9,32 +9,22 @@
 #include "opencv2/highgui/highgui.hpp"
 
 
-string room_test = "test";
-string room_test2 = "test2";
-string room_test3 = "test3";
-string room_test4 = "test4";
-string room_test5 = "AR1";
-PositionImgSetting pos_set1_1(room_test, 1);
-PositionImgSetting pos_set2_1(room_test2, 1);
-PositionImgSetting pos_set3_1(room_test3, 1);
-PositionImgSetting pos_set4_1(room_test4, 1);
-PositionImgSetting pos_set5_1(room_test5, 1);
+string room_final = "finalroom";
+PositionImgSetting PosSet1(room_final, 1);
 
 vector<Clue> clue_1;
 
 void SetAllClue()
 {
-	Clue clue0(room_test5, 1, "teddy", 0, 0, 0.0, 0.0, 0.1, 0.1), clue1;
+	Clue clue0(room_final, 1, "teddy", 0, 0, 0, 0), clue1;
 
 	clue0.add_dialog("This is a teddy!");
 	clue0.add_state(SHOW_ON_SCENE);
-	clue0.add_img_path("No image~~~");
+	clue0.add_2Dimg_path("No image~~~");
 
 	clue0.write_initial_file();
 
 	clue1.set_clue("D:\\clue\\AR1\\position1\\teddy");
-
-	int i;
 }
 
 
@@ -42,14 +32,14 @@ void SetResourceImg()
 {
 	int position_num = 1;
 
-	int findH_from = 618;
-	int findH_to = 619;
-	//choose_image(room_test5, position_num, VERTICAL_DOWN_40, findH_from, findH_to);
+	int findH_from = 32;
+	int findH_to = 33;
+	choose_image(room_final, position_num, VERTICAL_UP_20, findH_from, findH_to);
 
 	//resize_all(room_test5, position_num, VERTICAL_CENTRAL, 50);
 	//resize_all(room_test5, position_num, VERTICAL_UP_20, 50);
 	//resize_all(room_test5, position_num, VERTICAL_DOWN_20, 50);
-	resize_all(room_test5, position_num, VERTICAL_DOWN_40, 50);
+	//resize_all(room_test5, position_num, VERTICAL_DOWN_40, 50);
 }
 
 
@@ -61,9 +51,9 @@ void SetAllSceneImg()
 	tm.reset();
 	tm.start();
 
-	string path = "D:\\image\\image_use\\" + room_test3 + "\\position1" + "\\";
-	pos_set5_1.StitchSceneRange(VERTICAL_DOWN_20, 0, 156, METHOD2);
-	//pos_set5_1.StitchSceneAll(METHOD2);
+	string path = "D:\\image\\image_use\\" + room_final + "\\position1" + "\\";
+	PosSet1.StitchSceneRange(VERTICAL_DOWN_20, 0, 156, METHOD2);
+	//PosSet1.StitchSceneAll(METHOD2);
 
 	tm.stop();
 	cout << "process time = " << tm.getTimeSec() << " sec." << endl;
@@ -71,7 +61,7 @@ void SetAllSceneImg()
 
 void gameBuild()
 {
-	//SetResourceImgImg();
-	SetAllSceneImg();
+	SetResourceImg();
+	//SetAllSceneImg();
 	//SetAllClue();
 }
